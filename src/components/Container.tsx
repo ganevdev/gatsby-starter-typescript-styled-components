0f79ea7df44e6d655b985caf2e72e639db293421
Ivan Ganev <iganevru@gmail.com>
2018-09-16 01:27:47 +0300

    "semi": true

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,23 +1,23 @@
-import * as React from 'react'
-import styled from 'styled-components'
+import * as React from 'react';
+import styled from 'styled-components';
 
-import { widths } from '../styles/variables'
-import { getEmSize } from '../styles/mixins'
+import { widths } from '../styles/variables';
+import { getEmSize } from '../styles/mixins';
 
 const StyledContainer = styled.div`
-  position: relative;
-  margin-left: auto;
-  margin-right: auto;
-  width: auto;
-  max-width: ${getEmSize(widths.lg)}em;
-`
+    position: relative;
+    margin-left: auto;
+    margin-right: auto;
+    width: auto;
+    max-width: ${getEmSize(widths.lg)}em;
+`;
 
 interface ContainerProps {
-  className?: string
+    className?: string;
 }
 
 const Container: React.SFC<ContainerProps> = ({ children, className }) => (
-  <StyledContainer className={className}>{children}</StyledContainer>
-)
+    <StyledContainer className={className}>{children}</StyledContainer>
+);
 
-export default Container
+export default Container;
